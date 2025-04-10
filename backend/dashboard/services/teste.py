4115cbdbffc7466477a96f7f4a47d161ef84b3ca
Fagner Costa <fagner.costa@ifam.edu.br>
2025-04-10 14:17:26 -0400

    TESTE

--- backend/dashboard/services/teste.py
+++
@@ -1,4 +0,0 @@
-
-for posicao in range(1, 4):
-    print(f"AQUI{posicao}")
- 
--- frontend/src/types/metadata.ts
+++
@@ -1,4 +0,0 @@
-export const metadata = {
-    title: "Stencil Vision",
-    description: "Gerenciamento de stencils",
-};
